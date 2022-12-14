700efde022fd6365ac72cc25f6881394220ab610
Riccardo Gulin <riccardo.gulin@gmail.com>
2022-12-14 12:51:11 +0100

    Adds other DOM manipulation examples

--- index.js
+++ index.js
@@ -28,3 +28,51 @@ const cambiaTestoH1 = nuovoTesto => {
 
   h1.innerText = nuovoTesto // Sovrascrivo il testo attuale con il testo passato come parametro
 }
+
+const cambiaH1InBaseAValoreInput = () => {
+  const h1 = document.getElementsByTagName("h1")[0]
+  const inputField = document.getElementById("nuovo-testo-h1")
+
+  const nuovoContenuto = inputField.value // Leggo il valore attuale dell'input
+  h1.innerText = nuovoContenuto
+}
+
+const cambiaH2InArancione = () => {
+  // Seleziono tutti gli h2
+  const tuttiH2 = document.getElementsByTagName("h2") // Ottengo una HTML COLLECTION (simile agli array)
+  // Il forEach non funziona sulle html collection
+  // NON FUNZIONA --> tuttiH2.forEach(e => console.log(e))
+  const tuttiH2Array = Array.from(tuttiH2)
+  // Per ogni h2 vado ad aggiungere la classe scritte-arancioni
+  tuttiH2Array.forEach(h2 => h2.classList.toggle("scritte-arancioni"))
+
+  /*
+  
+  - .add("nomeClasse") aggiunge una classe
+  - .remove("nomeClasse") rimuove una classe
+  - .toggle("nomeClasse") aggiunge classe se non c'era, rimuove classe se c'era
+
+  */
+
+  /*   const tuttiH2 = document.querySelectorAll("h2")
+  tuttiH2.forEach(e => console.log(e))  Se seleziono col querySelectorAll posso usare forEach
+  */
+}
+
+const aggiungiElementoADiv = () => {
+  // Quando chiamata, crea un nuovo elemento ("div") da aggiungere come figlio al div preesistente
+
+  // 1. Creo il nuovo elemento/nodo
+  const nuovoDiv = document.createElement("div")
+
+  // 2. Modifico il nuovo elemento (aggiungo il testo, aggiungo una o più classi)
+  nuovoDiv.innerText = document.getElementById("nuovo-testo-h1").value
+
+  // 3. Devo ricordarmi di "agganciare" il nuovo elemento creato al DOM, selezionando un elemento padre al quale attaccarlo
+
+  // 3.1 Seleziono prima il padre
+  const padre = document.getElementById("lista-elementi")
+
+  // 3.2 Appendo il nuovoDiv al padre
+  padre.appendChild(nuovoDiv)
+}
